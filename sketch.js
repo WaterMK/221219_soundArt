c2a850bd0905f534e1bf5a98e5f409320951a8b0
WaterMK <watermin@duksung.ac.kr>
2022-12-20 23:07:23 +0900

    1220-8

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let sbCount=0;
 let vb;
 
 var note=[60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 0];
+var hueC=[0, 15, 30, 45, 60, 120, 170, 225, 250, 270, 295, 300, 345];
 
 var waves=[];
 var noteIndexes=[0, 0, 0];
@@ -29,6 +30,7 @@ function setup(){
 
 function draw(){
   colorMode(HSB);
+  stroke(0);
   background(255);
   
   for (var i=0;i<sb.length;i++){
@@ -103,6 +105,8 @@ function makeSound(n, x, y){
             freqHzes[0]=note[noteIndexes[0]]+vb;
             waves[0].freq(midiToFreq(freqHzes[0]));
             waves[0].amp(ampValue, 1);
+            
+            makeGraphic(n, m, sb[m].wb);
             break;
             
           case 1:
@@ -110,6 +114,8 @@ function makeSound(n, x, y){
             freqHzes[1]=note[noteIndexes[1]]+vb;
             waves[1].freq(midiToFreq(freqHzes[1]));
             waves[1].amp(ampValue, 1);
+            
+            makeGraphic(n, m, sb[m].wb);
             break;
             
           case 2:
@@ -117,6 +123,8 @@ function makeSound(n, x, y){
             freqHzes[2]=note[noteIndexes[2]]+vb;
             waves[2].freq(midiToFreq(freqHzes[2]));
             waves[2].amp(ampValue, 1);
+            
+            makeGraphic(n, m, sb[m].wb);
             break;
         }
       }
@@ -134,6 +142,65 @@ function makeSound(n, x, y){
   }
 }
 
+function makeGraphic(n, m, wb){
+  switch(n){
+    case 0:
+      noStroke();
+      fill(hueC[m], 100, 100);
+      rectMode(CENTER);
+      rect(width/2, width/2, width*0.8, width*0.8);
+      break;
+    
+    case 1:
+      noStroke();
+      if(wb) fill(0, 0, 0);
+      else if(!wb) fill(0, 0, 100);
+      star(width/2, width/2, width*0.4, width*0.25, 8);
+      
+      fill(hueC[m], 100, 100);
+      push();
+      translate(width/2, width/2);
+      for(var i=0;i<8;i++){
+        rotate(radians(45));
+        rectMode(CENTER);
+        rect(0, 0, width*0.2, width*0.8);
+      }
+      pop();
+      break;
+      
+    case 2:
+      noStroke();
+      if(wb) fill(0, 0, 0);
+      else if (!wb) fill(0, 0, 100);
+      push();
+      translate(width/2, width/2);
+      for(var j=0;j<8;j++){
+        rotate(radians(45));
+        ellipse(0, 0, width/8, width/3);
+      }
+      pop();
+      
+      fill(hueC[m], 100, 100);      
+      star(width/2, width/2, width*0.2, width*0.1, 8);
+      break;      
+  }  
+}
+
+function star(x, y, radius1, radius2, npoints){
+  let angle=TWO_PI/npoints;
+  let halfAngle=angle/2.0;
+  beginShape();
+  for (let a=0;a<TWO_PI;a+=angle){
+    let sx=x+cos(a)*radius2;
+    let sy=y+sin(a)*radius2;
+    vertex(sx, sy);
+    sx=x+cos(a+halfAngle)*radius1;
+    sy=y+sin(a+halfAngle)*radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
 document.addEventListener('gesturestart', function(e) {
   e.preventDefault();
 });
@@ -148,11 +215,14 @@ class soundButton {
     this.y2=this.y1+this.h;
     this.defaultColor=color(250);
     this.isPressed=false;
+    this.wb;
   }
   
   show(wb){
+    this.wb=wb;
     if(!wb) this.defaultColor=color(0);
     fill(this.defaultColor);
     rect(this.x1, this.y1, this.w, this.h);
   }
 }
+
